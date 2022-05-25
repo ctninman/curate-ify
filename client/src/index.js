c93fe903be159fb5734231b715195c3915806fba
ctninman <89664157+ctninman@users.noreply.github.com>
2022-05-25 15:32:47 -0400

    removed and reinstalled nodemodules

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
   // <React.StrictMode>
-    <App />
+    // <App />
   // </React.StrictMode>
-);
+// );
 
+// const root = document.getElementById("root");
+// root.render(<App />, root);
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
