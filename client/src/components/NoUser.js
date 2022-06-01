b942ff7d86137186a179a9c948474b6d41bca961
ctninman <89664157+ctninman@users.noreply.github.com>
2022-05-31 23:36:57 -0400

    test heroku update

--- client/src/components/NoUser.js
+++ client/src/components/NoUser.js
@@ -1,23 +1,66 @@
 import React from 'react';
 import LoadScreen from './LoadScreen';
+import FriendIcon from '../images/FriendIcon.png'
+import SpotifyIcon from '../images/spotify.png'
+import CollectionIcon from '../images/CollectionIcon.png'
+import QueueIcon from '../images/QueueIcon.png'
+import ListIcon from '../images/ListIcon.png'
 
 function NoUser(props) {
   return (
     <div className='flex-column-center'>
-      <h1 className='small-margins'>For Lovers of the Album</h1>
+      <h1 className='small-margins'>All About the Album</h1>
       <div className='flex-row-center' style={{width: '300px', margin: '20px'}}>
         <LoadScreen />
       </div>
-
-      <h2 className='small-margins'>1. Signup/Login</h2>
-      <h2 className='small-margins'>2. Connect to Your Spotify Account</h2>
-      <h2 className='small-margins'>3. Search Spotify for Albums</h2>
-      <h2 className='small-margins'>4. Add Albums To Your Collection with Genres and Tags</h2>
-      <h2 className='small-margins'>5. Place Albums You'd Like To Hear in Your Queue</h2>
-      <h2 className='small-margins'>6. Filter Your Collection to Easily Find An Album To Fit The Mood</h2>
-      <h2 className='small-margins'>7. Create Draggable Lists</h2>
-      <h2 className='small-margins'>8. Find Friends With Similar Collections</h2>
-
+      <div className='flex-row-center'>
+          <div style={{height: '35px'}}>
+            <h1 className='small-margins' style={{fontSize: 'x-large'}}>✅</h1>
+          </div>
+          <h2 className='small-margins'>1. Signup/Login</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{width: '35px'}}>
+            <img style={{width: '100%'}}src={SpotifyIcon}/>
+          </div>
+          <h2 className='small-margins'>2. Connect to Your Spotify Account</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{height: '35px'}}>
+            <h1 className='small-margins' style={{fontSize: 'x-large'}}>🔎</h1>
+          </div>
+          <h2 className='small-margins'>3. Search Spotify for Albums</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{width: '35px'}}>
+            <img style={{width: '100%'}}src={CollectionIcon}/>
+          </div>
+          <h2 className='small-margins'>4. Add Albums To Your Collection with Genres and Tags</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{height: '35px'}}>
+            <h1 className='small-margins' style={{fontSize: 'x-large'}}>🆕</h1>
+          </div>
+          <h2 className='small-margins'>5. Place Albums You'd Like To Hear in Your Queue</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{height: '35px'}}>
+            <h1 className='small-margins' style={{fontSize: 'x-large'}}>🎛️</h1>
+          </div>
+          <h2 className='small-margins'>6. Filter Your Collection to Easily Find An Album To Fit The Mood</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{height: '35px'}}>
+            <h1 className='small-margins' style={{fontSize: 'x-large'}}> 🔖</h1>
+          </div>
+          <h2 className='small-margins'>7. Create Draggable Lists</h2>
+        </div>
+        <div className='flex-row-center'>
+          <div style={{width: '35px'}}>
+            <img style={{width: '100%'}}src={FriendIcon}/>
+          </div>
+          <h2 className='small-margins'>8. Find Friends With Similar Collections</h2>
+        </div>
     </div>
   );
 }
